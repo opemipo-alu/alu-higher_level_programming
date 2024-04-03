13d23186d56e9a775c355c396ce862fab5a2f0ed
root <o.disu1@alustudent.com>
2024-04-03 09:56:46 -0700

    all

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-"
-str2 = "oriented "
-str3 = "programming "
-str4 = "with "
-str5 = "Python"
-print(str1 + str2 + str3 + str4 + str5)
 
+print("object-" + "oriented " + "programming " + "with " + "Python")
