141a47ec8a1a25ba828014aa4624af678b79be78
root <o.disu1@alustudent.com>
2024-04-03 10:15:26 -0700

    commit

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-print("object-" + "oriented " + "programming " + "with " + "Python")
-
-
+print(int("object oriented programming".find(" ")))  # Prints index of first space (19)
+print(int("python".find("o")))                         # Prints index of 'o' (1)
