43e28d91cc5018e47531526d6c0b08660752f688
root <o.disu1@alustudent.com>
2024-04-03 06:37:53 -0700

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,36 +3,28 @@
 class Square:
     """
     This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
     """
 
     def __init__(self, size):
         """
-        Initializes a new Square instance.
-
+        Initializes a new instance of the Square class.
         Args:
             size (int): The size of the square.
         """
         self.__size = size
 
 if __name__ == "__main__":
-    mysquare = Square(3)
-    print(type(mysquare))
-    print(mysquare.__dict__)
-
-    mysquare = Square(89)
-    print(type(mysquare))
-    print(mysquare.__dict__)
+    my_square = Square(3)
+    print(type(my_square))  # Output: <class '__main__.Square'>
+    print(my_square.__dict__)  # Output: {'_Square__size': 3}
 
     try:
-        print(mysquare.size)
+        print(my_square.size)  # Output: AttributeError: 'Square' object has no attribute 'size'
     except Exception as e:
         print(e)
 
     try:
-        print(mysquare._size)
+        print(my_square.__size)  # Output: AttributeError: 'Square' object has no attribute '__size'
     except Exception as e:
         print(e)
 
