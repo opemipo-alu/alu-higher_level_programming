353635fce2c0caff290a27df71f650f237a2c292
root <o.disu1@alustudent.com>
2024-04-03 12:41:07 -0700

    commit

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-print(int("object oriented programming".find(" ")))  # Get index of space
-print(int("python".find("o")))                         # Get index of 'o'
+str1 = "Welcome to"
+str2 = " Holberton School!"
+print(str1 + str2)'
