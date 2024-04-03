10c7e95dcff78eb83421b1e96802f147aed60e0f
root <o.disu1@alustudent.com>
2024-04-03 13:53:38 -0700

    all

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-print(int("object oriented programming".find(" ")))  # Prints index of first space (19)
-print(int("python".find("o")))                         # Prints index of 'o' (1)
+print("object" + "-" + "oriented programming with Python")
