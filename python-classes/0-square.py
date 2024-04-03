eff74553455c0e1fb349e9224029251bb0ce75a9
root <o.disu1@alustudent.com>
2024-04-03 06:01:28 -0700

    another commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
-class Square:
-        pass
+#!/usr/bin/python3
 
+class Square:
+    """
+    This is an empty class Square.
+    """
+
+    pass
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))  # Output: <class '0-square.Square'>
+    print(my_square.__dict__)  # Output: {}
