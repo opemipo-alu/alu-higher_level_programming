0e95202109898fbddf03c0729bb99cd7375c04fc
root <o.disu1@alustudent.com>
2024-04-05 02:32:24 -0700

    all

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "oriented programming with Python"
-print(str1, end="-")
-print(str2)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str))
